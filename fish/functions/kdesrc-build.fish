4d0a582b82dbe4d2a8dbce52cfe3287dbad70133
bailey brightman <baileybrightman@gmail.com>
2024-10-12 17:22:20 -0300

    Added kde build conf auto generated by bde-builder. Deleted stuff from omf.fish

--- fish/functions/kdesrc-build.fish
+++ fish/functions/kdesrc-build.fish
@@ -1,7 +1,7 @@
-
 # kdesrc-build #################################################################
 
 ## Add kdesrc-build to PATH
-set -x PATH $HOME/kde/src/kdesrc-build $PATH
+set -x PATH /home/m/kde/src/kdesrc-build $PATH
 
+set -x PATH /mnt/kde/kde/src/kdesrc-build $PATH
 
